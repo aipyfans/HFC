0ef4ebaba0e23aaab13f14daf856946165d1ecb7
william <lijunbo86@126.com>
2019-02-12 19:24:28 +0800

    Initial commit

---
+++ h01.h
@@ -0,0 +1,6 @@
+#ifndef HFC_HEAD_H
+#define HFC_HEAD_H
+
+void two_one_dot();
+
+#endif //HFC_HEAD_H
---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "h01.h"
+
+int main() {
+    printf("Hello, World!\n");
+
+    two_one_dot();
+
+    return 0;
+}
